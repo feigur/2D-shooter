ad38fb2fd9c88f5bad6db18850ec3f489de5c5e4
ofeigur95@gmail.com <4d3c2b1a>
2021-09-29 19:42:05 +0000

    init

---
+++ src/main/java/com/Hopur26/D/shooter/Application.java
@@ -0,0 +1,13 @@
+package com.Hopur26.D.shooter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
