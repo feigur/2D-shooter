a6d8772ff1a38d08e1375c69b49639d1db239b63
ofeigur95@gmail.com <4d3c2b1a>
2021-10-13 17:45:09 +0000

    test

--- src/main/java/controllers/MainMenuContoller.java
+++ src/main/java/controllers/MainMenuContoller.java
@@ -1,6 +1,15 @@
 package controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import storage.Entities.Account;
+
+@Controller
 public class MainMenuContoller {
+    private Account account;
 
-
+    @Autowired
+    public HomeController(Account account){
+        this.account = account;
+    }
 }
---
+++ src/main/java/storage/Entities/Account.java
@@ -0,0 +1,6 @@
+package storage.Entities;
+
+
+public class Account {
+
+}
