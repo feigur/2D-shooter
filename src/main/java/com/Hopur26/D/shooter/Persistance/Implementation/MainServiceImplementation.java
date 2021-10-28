418629efeecdc0320b696e00c67ac891cefe19fe
abj34 <abj34@hi.is>
2021-10-28 21:18:50 +0000

    main,settings,history - worked on -abj34

--- src/main/java/com/Hopur26/D/shooter/Persistance/Implementation/MainServiceImplementation.java
+++ src/main/java/com/Hopur26/D/shooter/Persistance/Implementation/MainServiceImplementation.java
@@ -2,7 +2,8 @@ package com.Hopur26.D.shooter.Persistance.Implementation;
 
 import com.Hopur26.D.shooter.Persistance.MainService;
 
-public class MainServiceImplementation implements MainService {
+/*public class MainServiceImplementation implements MainService {
 
 
 }
+*/
--- src/main/java/com/Hopur26/D/shooter/controllers/MainController.java
+++ src/main/java/com/Hopur26/D/shooter/controllers/MainController.java
@@ -5,8 +5,12 @@ import com.Hopur26.D.shooter.Persistance.MainService;
 import com.Hopur26.D.shooter.storage.Entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
@@ -21,4 +25,17 @@ public class MainController {
     public String mainGet(User user){
         return "main";
     }
+
+
+
+    @RequestMapping(value = "/settings", method = RequestMethod.GET)
+    public String settingsGet(User user){
+        return "settings";
+    }
+
+    @RequestMapping(value = "/history", method = RequestMethod.GET)
+    public String historyGet(User user){
+        return "history";
+    }
+
 }
