d538d113fd21cbad57c3eca0821f12973879ee4a
HinnDraugurinn <mao54@hi.is>
2021-11-22 14:46:09 +0000

    moved js out of html

--- src/main/resources/static/js/history.js
+++ src/main/resources/static/js/history.js
@@ -1,9 +1,9 @@
 //Get last5Games to work, says it is undefined
-var lastGame0 = document.getElementById("history0");
-var lastGame1 = document.getElementById("history1");
-var lastGame2 = document.getElementById("history2");
-var lastGame3 = document.getElementById("history3");
-var lastGame4 = document.getElementById("history4");
+//var lastGame0 = document.getElementById("history0");
+//var lastGame1 = document.getElementById("history1");
+//var lastGame2 = document.getElementById("history2");
+//var lastGame3 = document.getElementById("history3");
+//var lastGame4 = document.getElementById("history4");
 
 //function updateLists() {
 //    for(let i = 0; i<5;i++) {
@@ -12,3 +12,13 @@ var lastGame4 = document.getElementById("history4");
 //        document.getElementById("gameList").append(y);
 //    }
 //}
+
+//Get last5Games to work, says it is undefined
+
+function updateLists() {
+    for(let i = 0; i<last5Games.length;i++) {
+        var y = document.createElement("LI");
+        y.append(last5Games[i]);
+        document.getElementById("gameList").append(y);
+    }
+}
--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -1,4 +1,4 @@
-var playerName = "[[${LoggedInUser.username}]]";
+/*var playerName = "[[${LoggedInUser.username}]]";
 var playerButtons = [[${keys}]];
 
 function initButtons() {
@@ -9,7 +9,7 @@ function initButtons() {
         else{
             document.getElementById(i).innerHTML =  String.fromCharCode(playerButtons[i]);
         }
-        
+
     }
     document.getElementById("username").innerHTML = playerName;
 }
@@ -27,7 +27,7 @@ function updateUsername() {
 
 function updateButtons(c){
     var newButton = prompt("Change button to: ", playerButtons[parseInt(c)]);
-    
+
     if(newButton == null || newButton == "" || newButton.length > 1) {
         alert("One Button only!");
     } else {
@@ -39,6 +39,66 @@ function updateButtons(c){
             playerButtons[parseInt(c)] = button_id
             initButtons();
             debug.log(playerButtons);
+        }
+    }
+}*/
+
+
+//var playerName = "[[${LoggedInUser.username}]]";
+//var playerButtons = /*[[${keys}]]*/ "";
+
+function initButtons() {
+    for(let i = 0; i < playerButtons.length; i++) {
+        if (playerButtons[i] == 32){
+            document.getElementById(i).innerHTML =  "Space";
+        }
+        else{
+            document.getElementById(i).innerHTML =  String.fromCharCode(playerButtons[i]);
+        }
+
+    }
+    document.getElementById("username").innerHTML = playerName;
+}
+
+function updateUsername() {
+    var newName = prompt("Change username to: ", playerName);
+    if(newName == null || newName == ""){
+        alert("Username invalid!");
+    } else {
+        playerName = newName;
+        document.getElementById("newName").value = newName;
+        document.getElementById("username").innerHTML = newName;
+    }
+}
+
+function updateButtons(c){
+    var newButton = prompt("Change button to: ", playerButtons[parseInt(c)]);
+
+    if(newButton == null || newButton == "" || newButton.length > 1) {
+        alert("One Button only!");
+    } else {
+        button_id = parseInt(newButton.toUpperCase().charCodeAt(0));
+        if(playerButtons.includes(button_id)){
+            alert("Button in use");
+        }
+        else{
+            playerButtons[parseInt(c)] = button_id
+            if(c == 0){
+                document.getElementById("b0").value = button_id;
+            }
+            if(c == 1){
+                document.getElementById("b1").value = button_id;
+            }
+            if(c == 2){
+                document.getElementById("b2").value = button_id;
+            }
+            if(c == 3){
+                document.getElementById("b3").value = button_id;
+            }
+            if(c == 4){
+                document.getElementById("b4").value = button_id;
+            }
+            initButtons();
         }
     }
 }
